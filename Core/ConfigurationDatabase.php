4823adb2e03e692edf26037ab5ffcfd311ae923b
Umrohh <masfuumroh111@gmail.com>
2020-12-23 20:46:33 +0700

    menambahkan

---
+++ Core/ConfigurationDatabase.php
@@ -0,0 +1,11 @@
+<?php
+
+trait ConfigurationDatabase{
+private $HOSTNAME = "127.0.0.1";
+private $USERNAME = "root";
+private $PASSWORD = "";
+private $DBNAME = "baikinpesepeda";
+private $PORT = 3306;
+
+}
+?>
---
+++ Core/ControllerDB.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . "/MySQL.php";
+
+class ControllerDB {
+    protected $connection;
+
+    public function __construct() {
+        $mysql = new MySQL();
+        $this->connection = $mysql->getConnection();
+    }
+}
+?>
